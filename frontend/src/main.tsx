f65ab66e42db14cfac82155f3d6b4ca41e889ca0
Tarcísio Júnior <tarcisiosouzajrr@gmail.com>
2022-03-27 14:55:12 -0300

    first commit

---
+++ frontend/src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SignIn from './pages/SignIn'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <SignIn />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
